2b9a8dd64acd822a0632dcaa21057062910ff119
sakshinagpal <sakshi.nagpal@comprotechnologies.com>
2016-04-26 15:16:53 +0530

    Update index.js

--- test/index.js
+++ test/index.js
@@ -24,4 +24,8 @@ describe('mocha badge reporter', function() {
 		runner.emit('end');
 		stopCapture();
 	});
+	 it('should return -1 when the value is not present', function () {
+	      assert.equal(-1, [1,2,3].indexOf(5));
+	      assert.equal(-1, [1,2,3].indexOf(0));
+    	});
 });
