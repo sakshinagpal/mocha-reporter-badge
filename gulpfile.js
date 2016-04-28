6e3c944dda05b5633477a8952c8649fb08cec30e
sakshinagpal <sakshi.nagpal@comprotechnologies.com>
2016-04-28 12:01:02 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,9 @@ gulp.task('mkdir-build', function(callback){
 });
 
 gulp.task('test-badge', ['mkdir-build'], function(callback) {
-  exec('./node_modules/mocha/bin/_mocha --reporter ../../../index.js > build/mocha-badge.svg',callback);
+  exec('./node_modules/mocha/bin/_mocha --reporter ../../../index.js > build/mocha-badge.svg',function(err){
+	  console.log(err)
+  });
 });
 
 gulp.task('git-config', function(callback){
